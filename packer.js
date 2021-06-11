4fad379437f87d96ced6fc9ece2a8b8d8d3910ca
mehan <mehan.alavi.88.majd@gmail.com>
2021-06-11 19:27:31 +0430

    added packer

---
+++ packer.js
@@ -0,0 +1,16 @@
+const fs = require("fs")
+let data = "" ;
+
+let arrayOfJsfiles = [
+    "lib/Render/render.js",
+    "lib/Insert/inset.js"
+]
+
+for (const file in arrayOfJsfiles) {
+    let fileread = fs.readFileSync(file)
+    data += fileread + "\n"
+    console.log(`writing file ${file} is done`);
+}
+console.log("\n");
+console.log("everything is done");
+fs.appendFileSync("dist/applecake.js" , data )
