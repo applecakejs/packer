225a47603bc269ca743eb3057c1a99b65e632b19
mehan <74927578+mehanalavimajd@users.noreply.github.com>
2021-06-12 12:52:05 +0430

    Update packer.js

--- packer.js
+++ packer.js
@@ -1,18 +1,30 @@
-const fs = require("fs");
-let data = "";
-
-const Pack = (arrayOfJsfiles , dist , version) => {
-
-  for (const file of arrayOfJsfiles) {
-    let fileread = fs.readFileSync(file);
-    data += fileread + "\n";
-    console.log(`writing file ${file} is done`);
+{
+  "name": "applecake",
+  "version": "1.0.0-beta",
+  "description": "A JavaScript library to create html elements with js easily",
+  "main": "dist/applecake 1.0.0-alpha.js",
+  "directories": {
+    "lib": "lib"
+  },
+  "scripts": {
+    "test": "exit 0"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/applecakejs/applecake.git"
+  },
+  "keywords": [
+    "applecake",
+    "DOM",
+    "render"
+  ],
+  "author": "mehan alavimajd",
+  "license": "MIT",
+  "bugs": {
+    "url": "https://github.com/applecakejs/applecake/issues"
+  },
+  "homepage": "https://github.com/applecakejs/applecake#readme",
+  "dependencies": {
+    "chalk": "^4.1.1"
   }
-  console.log("\n");
-  console.log("everything is done");
-  fs.writeFileSync(`${dist} ${version}.js`, data);
-};
-
-module.exports = {
-    Pack
 }
