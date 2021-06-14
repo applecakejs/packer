71c6021d84e9a041e237c8f37ed93d37eadefd78
mehan <74927578+mehanalavimajd@users.noreply.github.com>
2021-06-14 11:35:20 +0430

    Creating "dist" directory by node.js

--- packer.js
+++ packer.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const chalk = require("chalk")
 let data = "";
 
-const Pack = (arrayOfJsfiles , dist , version) => {
+const Pack = (arrayOfJsfiles , filenamedist ) => {
 
   for (const file of arrayOfJsfiles) {
     let fileread = fs.readFileSync(file);
@@ -12,9 +12,11 @@ const Pack = (arrayOfJsfiles , dist , version) => {
   }
   console.log("\n");
   console.log(chalk.greenBright("everything is done"));
-  fs.writeFileSync(`${dist} ${version}.js`, data);
+  fs.mkdir("dist")
+  fs.writeFileSync(`${filenamedist}.js`, data);
 };
 
 module.exports = {
     Pack
 }
+fs.mkdir("dist")
